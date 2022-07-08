266a7169d053a1c14e955a8c1db336f752a07c69
kylebaugh <kylebaugh1@gmail.com>
2022-07-08 16:35:15 -0600

    asdf

--- server/controller.js
+++ server/controller.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
 
-const { CONNECTION_STRING } = process.env;
-const Sequelize = require("sequelize");
+// const { CONNECTION_STRING } = process.env;
+// const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(CONNECTION_STRING, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-});
+// const sequelize = new Sequelize(CONNECTION_STRING, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 module.exports = {
   createLog: (req, res) => {
-    const { date, miles } = body;
+    const { date, miles } = req.body;
 
     let newLog = {
       date: date,
       miles: miles,
     };
-    sequelize.query(`
-    INSERT INTO user_log (date, miles)
-    VALUES ('${date}', '${miles}')
-    `);
+
+    console.log(newLog)
+    // sequelize.query(`
+    // INSERT INTO user_log (date, miles)
+    // VALUES ('${date}', '${miles}')
+    // `);
   },
 };
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const  SERVER_PORT  = process.env.PORT;
+
 
 const { createLog } = require("./controller.js");
 
@@ -16,19 +16,19 @@ app.post("/api/log", createLog);
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/home.html"));
+  res.sendFile(path.join(__dirname, "../home.html"));
 });
 
 app.get("/css", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/home.css"));
+  res.sendFile(path.join(__dirname, "../home.css"));
 });
 
 app.get("/js", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/main.js"));
+  res.sendFile(path.join(__dirname, "../main.js"));
 });
 
-// const port = process.env.PORT || 5432;
+const port = process.env.PORT || 5432;
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Listening on port ${SERVER_PORT}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
